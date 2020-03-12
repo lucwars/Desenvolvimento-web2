e5a89d98e9dbe0455e54e28eab5d64422eb765d7
Lucas A <lucwars@hotmail.com>
2020-03-12 19:03:27 -0300

    Adicionando Componentes
    componentes adicionados html e css.

--- teste/Spotify/src/app/app.module.ts
+++ teste/Spotify/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ShowCaseComponent } from './show-case/show-case.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    ShowCaseComponent
   ],
   imports: [
     BrowserModule,
