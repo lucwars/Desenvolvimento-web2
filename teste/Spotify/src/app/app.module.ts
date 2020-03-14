13ac1eccf2ed280b45858ebe3454ef4e207c0ee3
Lucas Magalhaes <lucas.magalhaes@hp.com>
2020-03-14 13:00:42 -0300

    faq

--- teste/Spotify/src/app/app.module.ts
+++ teste/Spotify/src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { ShowCaseComponent } from './show-case/show-case.component';
-import { FootbarComponent } from './footbar/footbar.component';
-import { FaqComponent } from './faq/faq.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { ShowCaseComponent } from "./show-case/show-case.component";
+import { FootbarComponent } from "./footbar/footbar.component";
+import { FaqComponent } from "./faq/faq.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -16,11 +17,8 @@ import { FaqComponent } from './faq/faq.component';
     FootbarComponent,
     FaqComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
