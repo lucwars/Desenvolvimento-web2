9b3143dcc99d3a3b7e54afe4b1258a7476a4f96f
Lucas A <lucwars@hotmail.com>
2020-03-14 15:32:12 -0300

    Footer Completo.
    falta adicionar FAQ e data atual!

--- teste/Spotify/src/app/app-routing.module.ts
+++ teste/Spotify/src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { NavBarComponent } from "./nav-bar/nav-bar.component";
-import { ShowCaseComponent } from "./show-case/show-case.component";
-import { FaqComponent } from "./faq/faq.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  { path: "", component: ShowCaseComponent },
-  { path: "faq", component: FaqComponent }
-];
+
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- teste/Spotify/src/app/app.module.ts
+++ teste/Spotify/src/app/app.module.ts
@@ -1,24 +1,24 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { NavBarComponent } from "./nav-bar/nav-bar.component";
-import { ShowCaseComponent } from "./show-case/show-case.component";
-import { FootbarComponent } from "./footbar/footbar.component";
-import { FaqComponent } from "./faq/faq.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ShowCaseComponent } from './show-case/show-case.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     ShowCaseComponent,
-    FootbarComponent,
-    FaqComponent
+    FooterComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
