b7201b13de0e3ffbdc54e19af083e8f4e62ebfc0
Lucas Magalhaes <lucas.magalhaes@hp.com>
2020-03-18 08:29:25 -0300

    routes

--- teste/Spotify/src/app/app-routing.module.ts
+++ teste/Spotify/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { ShowCaseComponent } from "./show-case/show-case.component";
+import { FaqComponent } from "./faq/faq.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: ShowCaseComponent },
+  { path: "faq", component: FaqComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- teste/Spotify/src/app/footer/footer.component.ts
+++ teste/Spotify/src/app/footer/footer.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"]
 })
 export class FooterComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  getTodayDate(): String {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, "0");
+    var mm = String(today.getMonth() + 1).padStart(2, "0");
+    var yyyy = today.getFullYear();
+
+    return dd + "/" + mm + "/" + yyyy;
   }
-
 }
