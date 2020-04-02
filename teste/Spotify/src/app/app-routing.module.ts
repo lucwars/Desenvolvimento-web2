1d0cb504779483617878e505315d4a92e154eac0
Lucas Magalhaes <lucas.magalhaes@hp.com>
2020-04-02 18:03:18 -0300

    updated code

--- teste/Spotify/src/app/app-routing.module.ts
+++ teste/Spotify/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from "@angular/router";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { ShowCaseComponent } from "./show-case/show-case.component";
 import { FaqComponent } from "./faq/faq.component";
-import { from } from 'rxjs';
+import { CadastroComponent } from "./cadastro/cadastro.component";
+import { PlaylistComponent } from "./playlist/playlist.component";
+import { from } from "rxjs";
 
 const routes: Routes = [
   { path: "", component: ShowCaseComponent },
-  { path: "faq", component: FaqComponent }
+  { path: "faq", component: FaqComponent },
+  { path: "playlists", component: PlaylistComponent },
+  { path: "cadastro", component: CadastroComponent }
 ];
 
 @NgModule({
--- teste/Spotify/src/app/app.module.ts
+++ teste/Spotify/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ShowCaseComponent } from './show-case/show-case.component';
 import { FooterComponent } from './footer/footer.component';
+import { PlaylistComponent } from './playlist/playlist.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     ShowCaseComponent,
-    FooterComponent
+    FooterComponent,
+    PlaylistComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
