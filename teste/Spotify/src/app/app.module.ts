e1633cbbfda045a1dca6a2cc9db57eddcf27b98b
Lucas Magalhaes <lucas.magalhaes@hp.com>
2020-04-03 09:25:37 -0300

    form css & html

--- teste/Spotify/src/app/app.module.ts
+++ teste/Spotify/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { ShowCaseComponent } from './show-case/show-case.component';
-import { FooterComponent } from './footer/footer.component';
-import { PlaylistComponent } from './playlist/playlist.component';
-import { CadastroComponent } from './cadastro/cadastro.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { ShowCaseComponent } from "./show-case/show-case.component";
+import { FooterComponent } from "./footer/footer.component";
+import { PlaylistComponent } from "./playlist/playlist.component";
+import { CadastroComponent } from "./cadastro/cadastro.component";
 
 @NgModule({
   declarations: [
@@ -18,11 +18,8 @@ import { CadastroComponent } from './cadastro/cadastro.component';
     PlaylistComponent,
     CadastroComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
