4f5db79318f74600df866c474463b7436e291f17
Lucas A <lucwars@hotmail.com>
2020-04-13 18:13:31 -0300

    Adicionado todas as musicas, adicionado nav bar de musicas.
    adicionado window.scrollto para coordenar a posição de cada playlist de audio, adicionado instruções de voltar ao topo, adicionado botões neon

--- teste/Spotify/src/app/audioplayer/audioplayer.component.ts
+++ teste/Spotify/src/app/audioplayer/audioplayer.component.ts
@@ -8,8 +8,76 @@ import { Component, OnInit } from '@angular/core';
 export class AudioplayerComponent implements OnInit {
 
   constructor() { }
+  
 
   ngOnInit(): void {
+    window.scrollTo({
+      top: 0,
+      
+      behavior: 'smooth'
+    });
+  }
+
+  public openTop(){
+    
+    window.scrollTo({
+      top: 0,
+      
+      behavior: 'smooth'
+    });
+  }
+
+
+  public open(){
+    
+    window.scrollTo({
+      top: 140,
+      
+      behavior: 'smooth'
+    });
+  }
+
+  public open2(){
+    
+    window.scrollTo({
+      top: 832,
+      
+      behavior: 'smooth'
+    });
+  }
+
+  public open3(){
+    
+    window.scrollTo({
+      top: 1515,
+      
+      behavior: 'smooth'
+    });
+  }
+
+  public open4(){
+    
+    window.scrollTo({
+      top: 2206,
+      
+      behavior: 'smooth'
+    });
+  }
+  public open5(){
+    
+    window.scrollTo({
+      top: 2865,
+      
+      behavior: 'smooth'
+    });
+  }
+  public open6(){
+    
+    window.scrollTo({
+      top: 3590,
+      
+      behavior: 'smooth'
+    });
   }
 
 }
